8bd08a8b47cd34ae8d05fa5fc551fadd706f31cf
Vladislav <vladan982004@gmail.com>
2018-12-24 20:00:31 +0500

    Stable version of check login

--- js/check-login.js
+++ js/check-login.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
 	var checkLogin = (function(){
 
+		//Переменные
 		var _email = "mail@mail.com";
 		var _password = 123;
 
@@ -14,15 +15,11 @@ $(document).ready(function() {
 		var _passwordErrorBlock = $('#password-error-block');
 		var _emailPasswordWrongBlock = $('#email-password-wrong-block');
 
-		var _emptyEmail = 'empty-email';
-		var _wrongEmail = 'wrong-email';
-		var _emptyPassword = 'empty-password';
-		var _wrongEmailOrPassword = 'wrong-email-or-password';
-
 		var init = function(){
 			_setUpListeners();
 		}
 
+		//Прослушка события по submit
 		var _setUpListeners = function(){
 			_loginForm.on('submit', function(event){
 				_loginValidate(event);
@@ -34,95 +31,66 @@ $(document).ready(function() {
 
 			var form = _loginForm,
 				inputs = form.find('input'),
+				pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i,
 				emailVal = _emailInput.val().trim(),
-				passwordVal = _passwordInput.val().trim(),
-				fadeInEmail, fadeInPas, fadeInEmailPasswordBlock,
-				isWrongEmailFormatBlock = false;
+				passwordVal = _passwordInput.val().trim();
 
 			$.each(inputs, function(){
-
-				var DRY = function(prevDef, fadeInFor, blockName, blockNameAfter, remClass){
-					preventDef = prevDef ? event.preventDefault() : false;
-					fadeInFor = blockName.insertAfter(blockNameAfter);
-					blockNameAfter.css({'margin-bottom':'15px'});
-					fadeInFor.fadeIn(600);
-					blockName.removeClass(_emptyEmail);
-				}
-
+				
 				if (emailVal.length === 0 || passwordVal.length === 0) {
-					fadeInEmailPasswordBlock = _emailPasswordWrongBlock.addClass(_wrongEmailOrPassword);
-					fadeInEmailPasswordBlock.fadeOut(600);
 					if (emailVal.length === 0) {
 						event.preventDefault();
-						fadeInEmail = _emailErrorBlock.insertAfter(_emailInput);
-						_emailInput.css({'margin-bottom':'15px'});
-						fadeInEmail.fadeIn(600);
-						_emailErrorBlock.removeClass(_emptyEmail);
-					} else {
-						var pattern = /^([a-z0-9_\.-])+@[a-z0-9-]+\.([a-z]{2,4}\.)?[a-z]{2,4}$/i;
-						if (pattern.test(emailVal)){
-							fadeInEmailPasswordBlock = _emailPasswordWrongBlock.addClass(_wrongEmailOrPassword);
-							fadeInEmailPasswordBlock.fadeOut(600);
-							console.log('Email is VALID!');
-						} else {
-							event.preventDefault();
-							fadeInEmail = _wrongEmailFormatBlock.insertAfter(_emailInput);
-							fadeInEmailPasswordBlock = _emailPasswordWrongBlock.addClass(_wrongEmailOrPassword);
-							fadeInEmailPasswordBlock.fadeOut(600);
-							_emailInput.css({'margin-bottom':'15px'});
-							fadeInEmail.fadeIn(600);
-							_wrongEmailFormatBlock.removeClass(_wrongEmail);
-							isWrongEmailFormatBlock = true;
-							console.log('Email is INVALID!');
-						}
+						_emailPasswordWrongBlock.fadeOut(0);
+						_emailErrorBlock.fadeIn(600);
 					}
 
 					if (passwordVal.length === 0) {
-						fadeInEmailPasswordBlock = _emailPasswordWrongBlock.addClass(_wrongEmailOrPassword);
-						fadeInEmailPasswordBlock.fadeOut(600);
 						event.preventDefault();
-						fadeInPas = _passwordErrorBlock.insertAfter(_passwordInput);
-						_passwordInput.css({'margin-bottom':'15px'});
-						fadeInPas.fadeIn(600);
-						_passwordErrorBlock.removeClass(_emptyPassword);
+						_emailPasswordWrongBlock.fadeOut(0);
+						_passwordErrorBlock.fadeIn(600);
 					}
 				} else {
-					if (passwordVal != _password || emailVal != _email) {
+
+					if (!(pattern.test(emailVal))) {
 						event.preventDefault();
-						fadeInEmailPasswordBlock = _emailPasswordWrongBlock.insertBefore(_emailInput);
-						fadeInEmailPasswordBlock.fadeIn(600);
-						_emailPasswordWrongBlock.removeClass(_wrongEmailOrPassword);
+						_emailPasswordWrongBlock.fadeOut(0);
+						_wrongEmailFormatBlock.fadeIn(600);
+						console.log('Email is INVALID!');
+					} else if (pattern.test(emailVal) && emailVal != _email && passwordVal == _password) {
+						event.preventDefault();
+						_emailPasswordWrongBlock.fadeIn(600);
+						console.log('Email is INVALID!');
+					} else if (pattern.test(emailVal) && emailVal == _email && passwordVal != _password) {
+						event.preventDefault();
+						_emailPasswordWrongBlock.fadeIn(600);
+						console.log('Email is INVALID!');
+					} else if (pattern.test(emailVal) && emailVal != _email && passwordVal != _password) {
+						event.preventDefault();
+						_emailPasswordWrongBlock.fadeIn(600);
+						console.log('Email is INVALID!');
+					} else {
+						console.log('Email is VALID!');
 					}
 				}
 
 				_emailInput.on('focus', function(){
-					if(isWrongEmailFormatBlock === true)
-					{
-						fadeInEmail.fadeOut(600);
-						_wrongEmailFormatBlock.addClass(_wrongEmail);
-					} else {
-						fadeInEmail.fadeOut(600, function(){
-							_emailInput.css({'margin-bottom':'0px'});
-						});
-						_emailErrorBlock.addClass(_emptyEmail);
-					}
+					_emailErrorBlock.fadeOut(600);
+					_wrongEmailFormatBlock.fadeOut(600);
 				});
 
 				_passwordInput.on('focus', function(){
-					fadeInPas.fadeOut(600, function(){
-						_passwordInput.css({'margin-bottom':'0px'});
-					});
-					_passwordErrorBlock.addClass(_emptyPassword);
+					_passwordErrorBlock.fadeOut(600);
 				});
+
+
 			});
 		}
 
 		return {
 			init
 		}
-	
-	}());
-  
-	checkLogin.init();
 
+	}());
+
+	checkLogin.init();
 });
